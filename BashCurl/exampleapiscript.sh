60b0ae752fbea27af28e1ac4c138af13e3926332
Peter Brumblay <peter@tyrconsulting.com>
2013-09-17 11:21:37 -0600

    fixed example curl urls.

--- BashCurl/exampleapiscript.sh
+++ BashCurl/exampleapiscript.sh
@@ -19,7 +19,7 @@ curl -i -X DELETE -H "Authorization: Bearer $token" https://api.namebright.com/r
 curl -i -X PUT -H "Authorization: Bearer $token" -H "Content-Length: 0" https://api.namebright.com/rest/account/domains/example.com/nameservers/ns1.namebrightdns.com
 
 #change contact info
-curl -i -X PUT -H "Authorization: Bearer $token" -d Phone=303.555.1234 -d PostalCode=80203 -d FirstName=John -d LastName=Doe -d Email=johndoe@example.com -d "Address1=200 East Colfax Avenue" -d City=Denver -d Region=CO -d Country=US https://api.namebright.com/account/domains/example.com/contacts/technical
+curl -i -X PUT -H "Authorization: Bearer $token" -d Phone=303.555.1234 -d PostalCode=80203 -d FirstName=John -d LastName=Doe -d Email=johndoe@example.com -d "Address1=200 East Colfax Avenue" -d City=Denver -d Region=CO -d Country=US https://api.namebright.com/rest/account/domains/example.com/contacts/technical
 
 #change locking, auto-renew, or whois privacy
-curl -i -X PUT -H "Authorization: Bearer $token" -d DomainName=example.com -d Status=active -d ExpirationDate=2017-05-06T00:00:00 -d Locked=true -d AutoRenew=false -d WhoIsPrivacy=true -d "Category=Default Category" -d UpgradedDomain=false https://api.namebright.com/account/domains/example.com
+curl -i -X PUT -H "Authorization: Bearer $token" -d DomainName=example.com -d Status=active -d ExpirationDate=2017-05-06T00:00:00 -d Locked=true -d AutoRenew=false -d WhoIsPrivacy=true -d "Category=Default Category" -d UpgradedDomain=false https://api.namebright.com/rest/account/domains/example.com
